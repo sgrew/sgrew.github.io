a48cc6b232d6ecc4644fb60016e33c6ddbeadcbd
scottgrew949 <scottgrew949@cs408u-3e.acad.fullerton.edu>
2020-01-30 19:47:00 -0800

    Why did the programmer quit his job? Because he didn't get arrays...

--- prob-02/prob-02-main.cpp
+++ prob-02/prob-02-main.cpp
@@ -1,50 +1,37 @@
-// Nina Amenta
-// CPSC 120-XX
-// Lab-01, prob-01
-// 2020-01-29
-// nina.amenta@important.cs
+// Scott Grew
+// CPSC 120-05
+// Lab-01, prob-02
+// 2020-01-30
+// scottgrew949@csu.fullerton.edu
 //
 // Simple guessing game
-//
-
-// Your computer scientist of the day is Nina Amenta
-// https://en.wikipedia.org/wiki/Nina_Amenta
-// Invented key techniques in 3D surface reconstruction from points
-// and wrote her Ph.D. thesis on the relations between Helly's theorem
-// and generalized linear programming.
 
 #include <iostream>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-  // TODO: Declare a variable named secret_number and
-  //       hardcode the value to 42.
-  //       Remember the official style guide is online at
-  //       http://bit.ly/37Dl4UJ
 
-  // TODO: Declare a variable that will store the plauer's guess
+int secret_number = 42;
 
-  // TODO: Prompt the user for a guess and collect their guess
-  cout << "Guess a number: ";
+int guess;
 
+cout << "Guess a number: ";
 
-  // TODO: Print out the player's guess.
-  //       For example "You guessed 12, let's see if that's right..."
-  //       To build up tension, you can use the sleep function to 
-  //       put the program to sleep for a few seconds.
-  //       See https://manpages.ubuntu.com/manpages/disco/en/man3/sleep.3posix.html
-  
-  // TODO: If the guess is the same (or equal to) the secret_number 
-  //       print "Winner, winner, chicken dinner!\n"
+cin >> guess;
 
-  // TODO: Else if the the guess is less than the secret_number
-  //       print "Getting colder...\n"
+cout << "You guessed " << guess << " , Nice Try!" <<"\n";
 
-  // TODO: Else the guess must be greater than the secret_number
-  //       print "Too warm...\n"
+if (secret_number == guess) {
+  cout << "Come get a stamp champ!" <<"\n";
+}
+else if (secret_number > guess){
+  cout << "Sucks to suck..." <<"\n";
+}
 
-  // TODO: Remove all the TODO comments!
+else if (secret_number < guess){
+  cout << "Come on, try again..." <<"\n";
+}
 
   return 0;
 }
