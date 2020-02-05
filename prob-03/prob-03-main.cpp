615f3fc72b3e7e9b3d0ce3f86fee7b5770608141
Scott Grew <scottgrew949@csu.fullerton.edu>
2020-02-04 21:56:10 -0800

    When the inventor of the frisbee died, he was creamated and turned into a frisbee

--- prob-03/prob-03-main.cpp
+++ prob-03/prob-03-main.cpp
@@ -1,7 +1,7 @@
 // Scott Grew
 // CPSC 120-05
 // Lab-01, prob-03
-// 2020-01-29
+// 2020-02-04
 // scottgrew949@csu.fullerton.edu
 //
 
@@ -11,46 +11,36 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-  // TODO: Declare a variable named weight_in_pounds
-  //       hardcode the value to 42.
-  //       Remember the official style guide is online at
-  //       http://bit.ly/37Dl4UJ
+
 int weight_in_pounds = 42;
-  // TODO: Declare a variable named height_in_inches
+
 int height_in_inches;
-  // TODO: Declare a variable named age_in_years
+
 int age_in_years;
-  // TODO: Declare a variable named sex
-  //       Remember we're going to ask for a number in the program
-  //       and not a string.
+
 int sex;
-  // TODO: Declare a variable named bmr_adjustment
+
 int bmr_adjustment;
-  // TODO: Declare a variable bmr_estimate;
-  //       The units of bmr_estimate are kcal or kilocalories.
-  //       kilocalorie is the scientificly accurate name for
-  //       what we refer to as a calorie. One kcal is the amount
-  //       of energy needed to raise 1 kilogram (2.2 lbs) of water
-  //       by 1ºC (about 2ºF). A slice of pizza is about 300 kcal.
+
 int bmr_estimate;
 
-  // TODO: Prompt the user for their weight in pounds
-cout << "Enter weight in pounds\n";
+cout << "Enter weight in pounds:\n";
 cin >> weight_in_pounds;
-  // TODO: Prompt the user for their height in inches
-cout << "Enter height in inhes\n";
+
+cout << "Enter height in inches:\n";
 cin >> height_in_inches;
-// TODO: Prompt the user for their age in years
-cout << "Enter age in years\n";
+
+cout << "Enter age in years:\n";
 cin >> age_in_years;
-  // TODO: Prompt the user for their sex; ask the participant to
-  //       enter 1 for female and 2 for male.
-cout << "If female enter 1, if male enter 2\n";
+
+
+cout << "If female enter 1, if male enter 2:\n";
 cin >> sex;
 
 if (sex==1){
   bmr_adjustment = -161;
 }
+
 if (sex==2){
   bmr_adjustment = 5;
 }
@@ -59,8 +49,17 @@ if (sex==2){
   bmr = (10.0 * (weight_in_pounds / 2.205)) +
         (6.25 * (height_in_inches * 2.54)) -
         (5.0 * age_in_years) +
-        bmr_adjustment;
-  cout << bmr << "kcal\n";
+        bmr_adjustment; {
+          
+  cout << bmr << " kcal\n";
+
+  if (sex!=1 && sex!=2){
+     cout << "Hmmm, what are you then?\n";
+  }
+
+  cout << "Check the link below for more information:\n";
+  cout << "https://www.calculator.net/bmr-calculator.html\n";
+}
 
   return 0;
 }
